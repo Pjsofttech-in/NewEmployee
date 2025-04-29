d6b5cfb847640fcee8249445eca5b3b259da3d98
pooja patil <poojaspatil4322@gmail.com>
2025-04-29 10:17:22 +0530

    application.properties

---
+++ src/test/java/com/NewEmployeeManagement/NewEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewEmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
